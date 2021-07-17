3e2c65ce51a5d4111ca249f9bf69c8ad9bcec2fe
Archi711 <chwedczukarek@gmail.com>
2021-07-17 14:02:17 +0200

    fix/add 503 err handler

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -8,31 +8,38 @@ const initialState = {
   data: null,
   error: ERROR_CODES_MESSAGES[0]
 }
-export default function useFetch(url, method, headers, reqBody = null, OUTSIDE = false) {
-
-  const [body, setBody] = useState(reqBody)
-  const reqUrl = OUTSIDE ? OUTSIDE : `http://${process.env.REACT_APP_API_ADDRESS}${url}`
+export default function useFetch(
+  url,
+  method,
+  headers,
+  reqBody = null,
+  OUTSIDE = false
+) {
+  const [body, setBody] = useState(reqBody);
+  const reqUrl = OUTSIDE
+    ? OUTSIDE
+    : `http://${process.env.REACT_APP_API_ADDRESS}${url}`;
   const fetchReducer = (state, action) => {
     switch (action.type) {
       case 'request':
-        return { ...initialState, status: 'loading' }
+        return { ...initialState, status: 'loading' };
       case 'success':
-        return { ...initialState, status: 'success', data: action.payload }
+        return { ...initialState, status: 'success', data: action.payload };
       case 'failure':
-        return { ...initialState, status: 'error', error: action.payload }
+        return { ...initialState, status: 'error', error: action.payload };
       default:
-        return state
+        return state;
     }
-  }
+  };
 
-  const [state, dispatch] = useReducer(fetchReducer, initialState)
-  const cancelReq = useRef(false)
-  const cache = useRef([])
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+  const cancelReq = useRef(false);
+  const cache = useRef([]);
   useEffect(() => {
     const fetch = async () => {
-      dispatch({ type: 'request' })
+      dispatch({ type: 'request' });
       if (cache.current[url]) {
-        dispatch({ type: 'success', payload: cache.current[url] })
+        dispatch({ type: 'success', payload: cache.current[url] });
       }
       try {
         const response = await axios({
@@ -40,28 +47,31 @@ export default function useFetch(url, method, headers, reqBody = null, OUTSIDE =
           url: reqUrl,
           data: body,
           headers: headers,
-        })
-        cache.current[url] = response.data
-        dispatch({ type: 'success', payload: cache.current[url] })
-        if (cancelReq.current) return
-
+        });
+        cache.current[url] = response.data;
+        dispatch({ type: 'success', payload: cache.current[url] });
+        if (cancelReq.current) return;
+      } catch (err) {
+        dispatch({
+          type: 'failure',
+          payload:
+            ERROR_CODES_MESSAGES[
+              err.response?.status ? err.response.status : 503
+            ],
+        });
+        if (cancelReq.current) return;
       }
-      catch (err) {
-        dispatch({ type: 'failure', payload: ERROR_CODES_MESSAGES[err.response.status] })
-        if (cancelReq.current) return
-      }
-    }
-    if (body) fetch()
+    };
+    if (body) fetch();
     return () => {
-      cancelReq.current = true
-    }
+      cancelReq.current = true;
+    };
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [url, body])
+  }, [url, body]);
 
   // useEffect(() => {
   //   console.log(state)
   // }, [state])
 
-  return { state, setBody }
-
+  return { state, setBody };
 }
--- frontend/src/utils/errorMaker.js
+++ frontend/src/utils/errorMaker.js
@@ -2,18 +2,22 @@
 export const ERROR_CODES_MESSAGES = {
   0: {
     heading: '',
-    description: ''
+    description: '',
   },
   401: {
     heading: 'Błąd autoryzacji',
-    description: 'Sprawdź dane i spróbuj ponownie'
+    description: 'Sprawdź dane i spróbuj ponownie',
   },
   403: {
     heading: 'Błąd sesji',
-    description: 'Zaloguj się ponownie'
+    description: 'Zaloguj się ponownie',
   },
   500: {
     heading: 'Błąd serwera',
-    description: 'Spróbuj ponownie później'
-  }
-}
+    description: 'Spróbuj ponownie później',
+  },
+  503: {
+    heading: 'Serwer niedostępny',
+    description: 'Spróbuj ponownie później',
+  },
+};
